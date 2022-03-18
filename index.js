769eeed6945fb9685b67a544700d94f34d924cc6
JunaGanz <93413322+JunaGanz@users.noreply.github.com>
2022-03-18 10:05:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.
 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 
-global.db = new Low(new cloudDBAdacpter("https://json-server.junaganz.repl.co/"))
+global.db = new Low(new cloudDBAdapter("https://json-server.junaganz.repl.co/"))
 global.db.data = {
     users: {},
     chats: {},
