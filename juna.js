189df0ab60b1da21dd78d00bd9f29a6b221b99db
JunaGanz <93413322+JunaGanz@users.noreply.github.com>
2022-03-26 23:21:11 +0700

    Update juna.js

--- juna.js
+++ juna.js
@@ -2600,14 +2600,14 @@ juna.sendButtonText(m.chat, busa, 'Tetapi sebelum Kamu memulai game, apakah Kamu
                 let yts = require("yt-search")
 
                 let search = await yts(text)
-		var sec
-		
+
+                let sec = []
 
                 let no = 1
 
                 for (let i of search.all) {
 
-                sec += [{
+                res = {
 
 								"title": `࿈ No : ${no++} & Type : ${i.type}`,
 
@@ -2625,7 +2625,9 @@ juna.sendButtonText(m.chat, busa, 'Tetapi sebelum Kamu memulai game, apakah Kamu
 
 								]
 
-							}]
+							}
+
+				sec.push(res)			
 
                 }
 
@@ -2656,6 +2658,12 @@ juna.sendButtonText(m.chat, busa, 'Tetapi sebelum Kamu memulai game, apakah Kamu
             }
 
             break
+
+
+
+
+
+
             case 'jodohku': {
             if (!isPremium && global.db.data.users[m.sender].limit < 1) return reply(mess.endLimit)
             db.data.users[m.sender].limit -= 1 
